3b61d81e4a708c8427be28a17c0f4df5593cbb5b
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2021-05-03 20:53:07 +0300

    Remove usage of libbsd where not needed

--- src/utils.h
+++ src/utils.h
@@ -2,12 +2,7 @@
 #define ST_UTILS_H
 
 #include <stdbool.h>
-#include <stddef.h>
 
-/* string */
-bool st_ult_string_create(char **dst_str, size_t *dst_len, const char *src_str,
- size_t src_len);
-void st_utl_string_free_if_not_matching(char *str, const char *to_compare);
 bool st_utl_strings_equal(const char *left, const char *right);
 
 #endif
