38a2fd901b25efbe40218673fb30234a781bd9f1
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2021-05-04 15:16:42 +0300

    Fix function st_utl_strings_equal for conditions when one or both args are NULL

--- src/utils.c
+++ src/utils.c
@@ -3,5 +3,11 @@
 #include <string.h>
 
 bool st_utl_strings_equal(const char *left, const char *right) {
+    if (!left && !right)
+        return true;
+
+    if (!left || !right)
+        return false;
+
     return strcmp(left, right) == 0;
 }
