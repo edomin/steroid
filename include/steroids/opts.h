e16d94f50c62b19dd7b6f76e481ffdcb48a060ba
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2023-07-03 22:40:41 +0700

    Fix codestyle

--- include/steroids/opts.h
+++ include/steroids/opts.h
@@ -13,6 +13,7 @@
 ST_VISIBILITY st_modctx_t *st_opts_init(int argc, char **argv,
  st_modctx_t *logger_ctx);
 ST_VISIBILITY void st_opts_quit(st_modctx_t *opts_ctx);
+
 ST_VISIBILITY bool st_opts_add_option(st_modctx_t *opts_ctx, char short_option,
  const char *long_option, st_opt_arg_t arg, const char *arg_fmt,
  const char *option_descr);
