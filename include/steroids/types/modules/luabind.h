9770212e006b98e2fa73bf2fa2e578496b2531d2
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2023-07-17 18:54:51 +0700

    Codestyle fix

--- include/steroids/types/modules/luabind.h
+++ include/steroids/types/modules/luabind.h
@@ -10,8 +10,8 @@ typedef st_modctx_t *(*st_luabind_init_t)(st_modctx_t *logger_ctx,
 typedef void (*st_luabind_quit_t)(st_modctx_t *luabind_ctx);
 
 typedef struct {
-    st_luabind_init_t     luabind_init;
-    st_luabind_quit_t     luabind_quit;
+    st_luabind_init_t luabind_init;
+    st_luabind_quit_t luabind_quit;
 } st_luabind_funcs_t;
 
 #endif /* ST_STEROIDS_TYPES_MODULES_LUABIND_H */
