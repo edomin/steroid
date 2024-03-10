8ea5451f0230f538910adfa70e29d15c27f31ebf
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2024-03-11 01:19:29 +0600

    Add pathtools_cwalk module

--- include/steroids/types/modules/pathtools.h
+++ include/steroids/types/modules/pathtools.h
@@ -4,6 +4,13 @@
 
 #include "steroids/module.h"
 
+typedef enum {
+    ST_PT_IRI,
+    ST_PT_ABSOLUTE,
+    ST_PT_RELATIVE,
+    ST_PT_NOT_PATH,
+} st_pathtype_t;
+
 typedef st_modctx_t *(*st_pathtools_init_t)(st_modctx_t *logger_ctx);
 typedef void (*st_pathtools_quit_t)(st_modctx_t *pathtools_ctx);
 
