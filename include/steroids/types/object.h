b5330cd231032328e21b33cc00c31559aabce309
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2024-05-24 00:27:22 +0600

    Make object functions static

--- include/steroids/types/object.h
+++ include/steroids/types/object.h
@@ -17,7 +17,7 @@ typedef struct {
     void *funcs;
 } st_object_t;
 
-inline void st_object_make(void *object, void *owner, void *funcs) {
+static inline void st_object_make(void *object, void *owner, void *funcs) {
     ((st_object_t *)object)->owner = owner;
     ((st_object_t *)object)->funcs = funcs;
 }
@@ -26,6 +26,6 @@ static inline void *st_object_get_owner(const void *object) {
     return ((const st_object_t *)object)->owner;
 }
 
-inline void *st_object_get_funcs(void *object) {
+static inline void *st_object_get_funcs(void *object) {
     return ((st_object_t *)object)->funcs;
 }
